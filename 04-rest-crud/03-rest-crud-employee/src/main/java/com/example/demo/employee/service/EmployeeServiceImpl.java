0db07f91454df49c7a7b1c6f40981e7d457521af
blaxsior <heejun2798@gmail.com>
2024-03-14 22:45:40 +0900

    study: REST API 구현 및 service 아키텍처 추가

---
+++ 04-rest-crud/03-rest-crud-employee/src/main/java/com/example/demo/employee/service/EmployeeServiceImpl.java
@@ -0,0 +1,39 @@
+package com.example.demo.employee.service;
+
+import com.example.demo.employee.dao.EmployeeDAO;
+import com.example.demo.employee.entity.Employee;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+    private EmployeeDAO employeeDAO;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return this.employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        return this.employeeDAO.findById(id);
+    }
+    @Transactional
+    @Override
+    public Employee save(Employee employee) {
+        return this.employeeDAO.save(employee);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(int id) {
+        this.employeeDAO.deleteById2(id);
+    }
+}
