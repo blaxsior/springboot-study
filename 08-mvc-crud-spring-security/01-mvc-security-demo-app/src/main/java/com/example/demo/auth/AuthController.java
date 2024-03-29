69a8adb215391671d2bc30ebe7171b1fe21ccfe1
blaxsior <heejun2798@gmail.com>
2024-03-29 22:59:59 +0900

    study: form 로그인 + 로그아웃 실습

---
+++ 08-mvc-crud-spring-security/01-mvc-security-demo-app/src/main/java/com/example/demo/auth/AuthController.java
@@ -0,0 +1,16 @@
+package com.example.demo.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+    @GetMapping("/login")
+    public String loginPage() {
+        return "/auth/login";
+    }
+}
